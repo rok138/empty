5c891735c2a5ec9a9d3696b1f2d84fb0caa92e21
rok138 <rok138@163.com>
2020-01-01 20:56:14 +0800

    add 7

--- index/views.py
+++ index/views.py
@@ -3,4 +3,4 @@ from  django.http import HttpResponse
 
 # Create your views here.
 def indexView(request):
-    return HttpResponse("upload over!2211435680")
+    return HttpResponse("upload over!22114356780")
